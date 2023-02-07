e98e3794122e5ba6c8a281b17b4dd8a9ac90e177
Денис Гусельников <dguselnikov318@gmail.com>
2023-02-07 17:12:18 +0300

    register, breeze, userSettingApi

--- crm/app/Models/User.php
+++ crm/app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function usersettingapi()
+    {
+        return $this->hasOne(UserSettingApi::class);
+    }
 }
---
+++ crm/database/migrations/2023_02_07_084659_create_user_setting_apis_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserSettingApisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_setting_apis', function (Blueprint $table) {
+            $table->id();
+            $table->string('url');
+            $table->string('key');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_setting_apis');
+    }
+}
